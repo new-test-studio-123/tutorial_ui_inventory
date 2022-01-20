b403119bdbf12cb3f1883f3c5b1e3f02d55f0d51
AM Sultanov <neurofag@gmail.com>
2022-01-20 21:48:01 +0300

    adding drag and drop

--- Assets/Scripts/UIItem.cs
+++ Assets/Scripts/UIItem.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UIItem : MonoBehaviour
+public class UIItem : MonoBehaviour, IPointerDownHandler
 {
 
     public Item item;
     private Image spriteImage;
+    private UIItem selectedItem;
 
 
     void Awake() {
+        selectedItem = GameObject.Find("SelectedItem").GetComponent<UIItem>();
         spriteImage = GetComponent<Image>();
         Debug.Log("SpriteImage :" + spriteImage);
         UpdateItem(null);
@@ -28,5 +31,21 @@ public class UIItem : MonoBehaviour
             spriteImage.color = Color.clear;
         }
     }
-    
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       if (this.item != null){
+          if(selectedItem.item != null){
+              Item clone  = new Item(selectedItem.item);
+              selectedItem.UpdateItem(this.item);
+              UpdateItem(clone);
+          } else {
+              selectedItem.UpdateItem(this.item);
+              UpdateItem(null);
+          }
+       } else if (selectedItem.item != null) {
+           UpdateItem(selectedItem.item);
+           selectedItem.UpdateItem(null);
+       }
+    }
 }
